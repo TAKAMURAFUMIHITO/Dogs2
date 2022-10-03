91059d895e2a759741458ad5fd2b7bc3b087fd0d
EC2 Default User <ec2-user@ip-172-31-43-22.ap-northeast-1.compute.internal>
2022-10-03 14:46:07 +0000

    [Add]会員一覧,詳細画面

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,19 +1,47 @@
 class Public::MembersController < ApplicationController
+  before_action :ensure_correct_member, only: [:update]
   def index
+    @members = Member.all
   end
 
   def show
+    @member = Member.find(params[:id])
   end
 
   def edit
+    @member = Member.find(params[:id])
+    if @member == current_member
+      render :edit
+    else
+      redirect_to member_path(current_member)
+    end
   end
 
   def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to member_path(@member.id), notice: "プロフィールを更新しました。"
+    else
+      render "edit"
+    end
   end
 
   def confirm
   end
 
   def withdraw
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_member
+    @member = Member.find(params[:id])
+    unless @menber == current_member
+      redirect_to member_path(current_member)
+    end
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,11 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, length: { minimum: 2, maximum: 20 }, presence: true
+  has_one_attached :profile_image
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no-image.PNG'
+  end
 end
