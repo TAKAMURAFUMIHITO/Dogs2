f2677a46dbb5eb4a3108342a8e72ac5d469494e6
EC2 Default User <ec2-user@ip-172-31-43-22.ap-northeast-1.compute.internal>
2022-10-11 14:15:45 +0000

    [Add]いいね一覧機能

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module Public::NotificationsHelper
 
   def notification_form(notification)
@@ -26,29 +25,3 @@ module Public::NotificationsHelper
   end
 
 end
-=======
-module Public::NotificationsHelper
-
-  def notification_form(notification)
-    @visitor = notification.visitor
-    @comment = nil
-    your_item = link_to 'あなたの投稿', post_path(notification), style: "font-weight: bold;"
-    @visitor_comment = notification.comment_id
-    #notification.actionがfollowかlikeかcommentか
-    case notification.action
-      when "follow" then
-        tag.a(notification.visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"があなたをフォローしました"
-      when "like" then
-        tag.a(notification.visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"が"+tag.a('あなたの投稿', href: post_path(notification.post_id), style: "font-weight: bold;")+"にいいねしました"
-      when "comment" then
-        comment = Comment.find_by(id: @visitor_comment)&.content
-      tag.a(@visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"が"+tag.a('あなたの投稿', href: post_path(notification.post_id), style: "font-weight: bold;")+"にコメントしました"
-    end
-	end
-
-	def unchecked_notifications
-    @notifications = current_member.passive_notifications.where(checked: false)
-  end
-
-end
->>>>>>> origin/develop
